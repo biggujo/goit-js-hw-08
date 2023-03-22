b947ce1db21c6cb09b092eec3af23421ce171085
biggujo <gen.style22@gmail.com>
2023-03-22 18:55:00 +0200

    Task 3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,65 @@
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+let formData = {};
+
+const refs = {
+  form: document.querySelector(".feedback-form"),
+}
+
+refs.form.addEventListener("input", throttle(onFormInput, 500));
+refs.form.addEventListener("submit", onSubmit);
+
+loadData();
+
+function onFormInput(e) {
+  saveText(e.target);
+  saveData();
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  refs.form.reset()
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function saveText(target) {
+  formData[target.name] = target.value;
+}
+
+function saveData() {
+  try {
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+  }
+  catch (e) {
+    console.log("Error!");
+    console.log(e.name);
+    console.log(e.message);
+  }
+}
+
+function loadData() {
+  try {
+    const parseResult = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+    if (parseResult === null) {
+      return;
+    }
+
+    formData = parseResult;
+
+    // Fill form elements with data
+    [...refs.form.elements].forEach(el => {
+      if (formData[el.name] === undefined) {
+        return;
+      }
+      el.value = formData[el.name];
+    });
+  }
+  catch (e) {
+    console.log("Error!");
+    console.log(e.name);
+    console.log(e.message);
+  }
+}
