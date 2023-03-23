8ec264012fc838a6fa0a40c1bc9afe72fcd3cc2d
biggujo <gen.style22@gmail.com>
2023-03-23 18:57:35 +0200

    Fix all things

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,4 @@
-import throttle from 'lodash.throttle';
+import throttle from "lodash.throttle";
 
 const LOCALSTORAGE_KEY = "feedback-form-state";
 
@@ -6,33 +6,51 @@ let formData = {};
 
 const refs = {
   form: document.querySelector(".feedback-form"),
-}
+};
 
 refs.form.addEventListener("input", throttle(onFormInput, 500));
 refs.form.addEventListener("submit", onSubmit);
 
 loadData();
 
-function onFormInput(e) {
-  saveText(e.target);
+function onFormInput() {
   saveData();
 }
 
 function onSubmit(e) {
   e.preventDefault();
-  refs.form.reset()
+
+  saveData();
+
+  if (!isFormFilled()) {
+    return;
+  }
+
+  console.log("The data has been send!");
+  console.table(formData);
+  formData = {};
+
+  refs.form.reset();
   localStorage.removeItem(LOCALSTORAGE_KEY);
 }
 
-function saveText(target) {
-  formData[target.name] = target.value;
+function isFormFilled() {
+  return Object.values(formData).every(val => val !== "");
 }
 
 function saveData() {
   try {
+    // Save all data to the object
+    [...refs.form.elements].forEach(el => {
+      if (el.nodeName === "BUTTON") {
+        return;
+      }
+
+      formData[el.name] = el.value;
+    });
+
     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
-  }
-  catch (e) {
+  } catch (e) {
     console.log("Error!");
     console.log(e.name);
     console.log(e.message);
